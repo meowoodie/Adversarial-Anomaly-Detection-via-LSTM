a9246fce24312d1ce71fea62c0a1c9bc5d64e43a
meowoodie <woodielove@163.com>
2019-09-03 22:52:20 -0400

    init commit

---
+++ pprnn.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Recurrent Neural Networks for Point Processes.
+"""
+
+import sys
+import arrow
+import numpy as np
+import tensorflow as tf
+
+class PointProcessRNN(object):
+    """
+    """
+
+    def __init__(self, ):
+        """
+        """
+        pass
