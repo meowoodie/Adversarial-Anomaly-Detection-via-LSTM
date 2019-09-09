06952fbb228a1754199233772feba9fb33de4bea
meowoodie <woodielove@163.com>
2019-09-09 00:00:03 -0400

    basic structure of rnn, except for the thinning procedure

--- pprnn.py
+++ pprnn.py
@@ -14,7 +14,85 @@ class PointProcessRNN(object):
     """
     """
 
-    def __init__(self, ):
+    def __init__(self, step_size):
         """
+        Params:
+        """
+        # model hyper-parameters
+        self.lstm_hidden_size  = lstm_hidden_size # size of hidden states
+        self.step_size         = step_size        # step size of LSTM
+        self.y_len             = y_len            # length of a single output
+
+        # # define learning weights
+        # # - time weights
+        # self.Wt  = tf.get_variable(name="Wt", initializer=INIT_PARAM_RATIO * tf.random_normal([self.lstm_hidden_size, self.t_dim]))
+        # self.bt  = tf.get_variable(name="bt", initializer=INIT_PARAM_RATIO * tf.random_normal([self.t_dim]))
+        # # - location weights
+        # self.Wl0 = tf.get_variable(name="Wl0", initializer=INIT_PARAM_RATIO * tf.random_normal([self.lstm_hidden_size, self.loc_hidden_size]))
+        # self.bl0 = tf.get_variable(name="bl0", initializer=INIT_PARAM_RATIO * tf.random_normal([self.loc_hidden_size]))
+        # self.Wl1 = tf.get_variable(name="Wl1", initializer=INIT_PARAM_RATIO * tf.random_normal([self.loc_hidden_size, self.loc_param_size]))
+        # self.bl1 = tf.get_variable(name="bl1", initializer=INIT_PARAM_RATIO * tf.random_normal([self.loc_param_size]))
+        # # - mark weights
+        # self.Wm0 = tf.get_variable(name="Wm0", initializer=INIT_PARAM_RATIO * tf.random_normal([self.lstm_hidden_size, self.mak_hidden_size]))
+        # self.bm0 = tf.get_variable(name="bm0", initializer=INIT_PARAM_RATIO * tf.random_normal([self.mak_hidden_size]))
+        # self.Wm1 = tf.get_variable(name="Wm1", initializer=INIT_PARAM_RATIO * tf.random_normal([self.mak_hidden_size, self.m_dim]))
+        # self.bm1 = tf.get_variable(name="bm1", initializer=INIT_PARAM_RATIO * tf.random_normal([self.m_dim]))
+
+    def initialize_network(self, batch_size):
+        """Create a new network for training purpose, where the LSTM is at the zero state"""
+        # create a basic LSTM cell
+        tf_lstm_cell    = tf.nn.rnn_cell.BasicLSTMCell(self.lstm_hidden_size)
+        # defining initial basic LSTM hidden state [2, batch_size, lstm_hidden_size]
+        # - lstm_state.h: hidden state [batch_size, lstm_hidden_size]
+        # - lstm_state.c: cell state   [batch_size, lstm_hidden_size]
+        init_lstm_state = tf_lstm_cell.zero_state(batch_size, dtype=tf.float32)
+        # construct customized LSTM network
+        self.output self.loglik, self.final_state = self._recurrent_structure(
+            batch_size, tf_lstm_cell, init_lstm_state)
+
+    def _recurrent_structure(self, 
+            batch_size, 
+            tf_lstm_cell,     # tensorflow LSTM cell object, e.g. 'tf.nn.rnn_cell.BasicLSTMCell'
+            init_lstm_state): # initial LSTM state tensor
+        """Recurrent structure with customized LSTM cells."""
+        # init_o: initial output [batch_size, y_len] 
+        init_o = tf.zeros([batch_size, self.y_len], dtype=tf.float32)
+        # concatenate each customized LSTM cell by loop
+        output = []
+        loglik = []
+        last_y, last_lstm_state = init_o, init_lstm_state # loop initialization
+        for _ in range(self.step_size):
+            y, l, state = self._customized_lstm_cell(batch_size, tf_lstm_cell, last_lstm_state, last_y)
+            output.append(y)        # record single output
+            loglik.append(l)        # record likelihood
+            last_y          = y     # reset last_y
+            last_lstm_state = state # reset last_lstm_state
+        output = tf.stack(output, axis=1) # [batch_size, step_size, y_len]
+        loglik = tf.stack(loglik, axis=1) # [batch_size, step_size, 1]
+        return output, loglik, state
+
+    def _customized_lstm_cell(self, batch_size, 
+            tf_lstm_cell, # tensorflow LSTM cell object, e.g. 'tf.nn.rnn_cell.BasicLSTMCell'
+            last_state,   # last state as input of this LSTM cell
+            last_t):      # last_t + delta_t as input of this LSTM cell
+        """
+        Customized Stochastic LSTM Cell
+        The customized LSTM cell takes external input and the hidden state of the last moment
+        as input, then return the external output as well as the hidden state at the next moment. 
+        The reason that avoids using tensorflow builtin rnn structure is that, 
+        """
+        # stochastic single output and its likelihood
+        y, l = self._sample_y(batch_size, last_state.h) # [batch_size, x_sample], [batch_size, 1] 
+        # one step rnn structure
+        # - y is a tensor that contains a single step of data points with shape [batch_size, y_len]
+        # - state is a tensor of hidden state with shape [2, batch_size, state_size]
+        _, next_state = tf.nn.static_rnn(tf_lstm_cell, [y], initial_state=last_state, dtype=tf.float32)
+        return y, l, next_state
+
+    def _sample_y(self, batch_size, last_state):
+        """
+        Sample Single Output and Its Likelihood Value
+        Given the last hidden state of the RNN, the function samples a single output based on 
+        the intensity function which is defined by the hidden state. 
         """
         pass
