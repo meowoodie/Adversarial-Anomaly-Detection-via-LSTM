41c120489bef89ecf7048eade845597b77e6a57b
meowoodie <woodielove@163.com>
2019-09-12 13:18:55 -0400

    update sample_ts

--- pprnn.py
+++ pprnn.py
@@ -14,15 +14,14 @@ class PointProcessRNN(object):
     """
     """
 
-    def __init__(self, step_size, lstm_hidden_size, ts_len):
+    def __init__(self, step_size, lstm_hidden_size):
         """
-        Params:
+        Args:
         """
-        INIT_PARAM_RATIO = 1e-2
+        INIT_PARAM_RATIO = 1#1e-2
         # model hyper-parameters
         self.lstm_hidden_size  = lstm_hidden_size # size of hidden states
         self.step_size         = step_size        # step size of LSTM
-        self.ts_len            = ts_len           # length of a single output
         self.mu                = 0
 
         # # define model weights
@@ -49,27 +48,27 @@ class PointProcessRNN(object):
         # - lstm_state.h: hidden state [batch_size, lstm_hidden_size]
         # - lstm_state.c: cell state   [batch_size, lstm_hidden_size]
         init_lstm_state = tf_lstm_cell.zero_state(batch_size, dtype=tf.float32)
-        # init_ts: initial output [batch_size, ts_len]
-        init_ts         = tf.zeros([batch_size, self.ts_len], dtype=tf.float32)
+        # init_t: initial output [batch_size, 1]
+        init_t          = tf.zeros([batch_size], dtype=tf.float32)
         # concatenate each customized LSTM cell by loop
-        output = []
-        loglik = []
-        last_ts, last_lstm_state = init_ts, init_lstm_state # loop initialization
+        outputs = [] # [batch_size, step_size, 3]
+        logliks = [] # [batch_size, step_size, 1]
+        last_t, last_lstm_state = init_t, init_lstm_state # loop initialization
         for _ in range(self.step_size):
-            ts, l, state = self._customized_lstm_cell(batch_size, tf_lstm_cell, last_lstm_state, last_ts)
-            output.append(ts)       # record single output
-            loglik.append(l)        # record likelihood
-            last_ts         = ts    # reset last_ts
-            last_lstm_state = state # reset last_lstm_state
-        output = tf.stack(output, axis=1) # [batch_size, step_size, ts_len]
-        # loglik = tf.stack(loglik, axis=1) # [batch_size, step_size, 1]
-        return output, loglik, state
+            ts, loglik, state = self._customized_lstm_cell(batch_size, tf_lstm_cell, last_lstm_state, last_t)
+            outputs.append(ts)            # record single output  [batch_size, 1, 3]
+            logliks.append(loglik)        # record likelihood     [batch_size, 1, 1]
+            last_t          = ts[:, 0]    # reset last_ts         [batch_size]
+            last_lstm_state = state       # reset last_lstm_state [batch_size, lstm_hidden_size]
+        outputs = tf.stack(outputs, axis=1) # [batch_size, step_size, 3]
+        logliks = tf.stack(logliks, axis=1) # [batch_size, step_size, 1]
+        return outputs, logliks, state
 
     def _customized_lstm_cell(self, 
             batch_size, 
             tf_lstm_cell,    # tensorflow LSTM cell object, e.g. 'tf.nn.rnn_cell.BasicLSTMCell'
             last_lstm_state, # last state as input of this LSTM cell
-            last_t):         # last_t + delta_t as input of this LSTM cell
+            last_t):        # last_t + delta_t as input of this LSTM cell
         """
         Customized Stochastic LSTM Cell
         The customized LSTM cell takes external input and the hidden state of the last moment
@@ -77,72 +76,83 @@ class PointProcessRNN(object):
         The reason that avoids using tensorflow builtin rnn structure is that, 
         """
         # stochastic single output and its likelihood
-        ts, l = self._sample_ts(batch_size, last_lstm_state.h) # [batch_size, ts_len], [batch_size, 1]
+        ts     = self._sample_ts(batch_size, last_t, last_lstm_state.h) # [batch_size, 3]
+        # calculate log likelihood of above single output
+        loglik = self._log_likelihood(ts, last_lstm_state.h)            # [batch_size, 1]
         # one step rnn structure
-        # - ts is a tensor that contains a single step of data points with shape [batch_size, ts_len]
-        # - state is a tensor of hidden state with shape [2, batch_size, state_size]
+        # - ts is a tensor that contains a single step of data points with shape [batch_size, 3]
+        # - state is a tensor of hidden state with shape                         [2, batch_size, state_size]
         _, next_state = tf.nn.static_rnn(tf_lstm_cell, [ts], initial_state=last_lstm_state, dtype=tf.float32)
-        return ts, l, next_state
+        return ts, loglik, next_state
 
-    def _sample_ts(self, batch_size, lstm_state, n_sample=1000, upperb=1000):
+    def _sample_ts(self, batch_size, last_t, lstm_state, n_sample=1000, upperb=1000):
         """
         Sample Single Output and Its Likelihood Value
         Given the last hidden state of the RNN, the function samples a single output based on 
         the intensity function which is defined by the hidden state. 
         """
-        # generate random spatio-temporal points in space ([0, 1], [0, 1], [0, 1])
-        cand_t  = tf.random.uniform(shape=[batch_size, n_sample, 1], minval=0, maxval=1, dtype=tf.float32)
-        cand_t  = tf.contrib.framework.sort(cand_t, axis=1) # sort the random points in chronological order
-        cand_s  = tf.random.uniform(shape=[batch_size, n_sample, 2], minval=-1, maxval=1, dtype=tf.float32)
-        cand_ts = tf.concat([cand_t, cand_s], axis=2)
-        # generate acceptence rate matrix [batch_size, n_sample]
-        accept  = tf.random.uniform(shape=[batch_size, n_sample, 1], minval=0, maxval=1, dtype=tf.float32)
         # calculate lambda (conditional intensity) for each batch
-        ts      = []
+        ts = [] # [batch_size, 3]
         for b in range(batch_size):
-            # candidates preparation
-            b_ts_cand    = cand_ts[b, :, :]                                         # [n_sample, 3]
-            b_lstm_state = tf.expand_dims(lstm_state[b, :], -1)                     # [lstm_hidden_size]
-            b_accept     = accept[b, :, :]                                          # [n_sample, 1]
-            b_lam        = self.mu + tf.exp(tf.linalg.matmul(tf.linalg.matmul(b_ts_cand, self.K), b_lstm_state)) # [n_sample, 1]
+            # generate random spatio-temporal points in space ([0, 1], [0, 1], [0, 1])
+            cand_t  = tf.random.uniform(shape=[n_sample, 1], minval=last_t[b], maxval=1, dtype=tf.float32)
+            cand_t  = tf.contrib.framework.sort(cand_t, axis=0) # sort the random points in chronological order
+            cand_s  = tf.random.uniform(shape=[n_sample, 2], minval=-1, maxval=1, dtype=tf.float32)
+            cand_ts = tf.concat([cand_t, cand_s], axis=1)
+            # generate acceptence rate matrix [batch_size, n_sample]
+            accept  = tf.random.uniform(shape=[n_sample, 1], minval=0, maxval=1, dtype=tf.float32)
+            # calculate lambda for each sample
+            state   = tf.expand_dims(lstm_state[b, :], -1) # [lstm_hidden_size, 1]
+            lam     = self.__lambda(cand_ts, state)        # [n_sample, 1]
             # reject samples
-            b_mask = tf.squeeze(tf.cast(b_accept * upperb > b_lam, dtype=tf.int32)) # [n_sample]
+            mask    = tf.squeeze(tf.cast(accept * upperb > lam, dtype=tf.int32))                   # [n_sample]
             # get first non-zero sample
             # NOTE: 
             # the shape of return tensor of tf.gather cannot be inferred, which will lead to a ValueError
             # (Cannot iterate over a shape with unknown rank). Because, in tf.nn.rnn_cell.BasicLSTMCell,
             # the function requires the shape of inputs should be inferred via shape inference, as shown 
             # in https://www.tensorflow.org/api_docs/python/tf/nn/static_rnn
-            b_ts   = tf.gather_nd(
-                b_ts_cand,
-                tf.where(tf.not_equal(b_mask, 0)))[0]
+            b_ts = tf.gather_nd(
+                cand_ts,
+                tf.where(tf.not_equal(mask, 0)))[0]
             ts.append(b_ts)
         ts = tf.stack(ts)
-        return ts, 0
+        return ts
+
+    def __lambda(self, ts, lstm_state):
+        """
+        conditional intensity given history embedding `lstm_state` and current point `ts`
+        """
+        return self.mu + tf.exp(tf.linalg.matmul(tf.linalg.matmul(ts, self.K), lstm_state))
+
+    def _log_likelihood(self, ts, lstm_state):
+        """
+        log likelihood given history embedding `lstm_state` and current point `ts`
+        """
+        return tf.random.uniform(shape=[5, 1], minval=-1, maxval=1, dtype=tf.float32)
 
     def train(self, sess, batch_size):
         """
         """
         # define network structure
-        self.output, self.loglik, self.final_state = self._recurrent_structure(batch_size)
+        self.outputs, self.logliks, self.final_state = self._recurrent_structure(batch_size)
         # initialize variables
         init_op = tf.global_variables_initializer()
         sess.run(init_op)
         # 
-        print(sess.run(self.output))
+        print(sess.run(self.outputs))
 
     # def debug(self, sess, batch_size):
     #     tf_lstm_cell = tf.nn.rnn_cell.BasicLSTMCell(self.lstm_hidden_size)
     #     lstm_state   = tf_lstm_cell.zero_state(batch_size, dtype=tf.float32)
-    #     ts, l = self._sample_ts(batch_size, lstm_state=lstm_state.h, n_sample=10, upperb=2)
-    #     # ts  = tf.random.uniform(shape=[batch_size, 3], minval=0, maxval=1, dtype=tf.float32)
-    #     res = tf.nn.static_rnn(tf_lstm_cell, [ts], initial_state=lstm_state, dtype=tf.float32)
+    #     ts = self._sample_ts(batch_size, last_t=[0, .88, .88, .99, .99], lstm_state=lstm_state.h, n_sample=10, upperb=2)
+    #     # res = tf.nn.static_rnn(tf_lstm_cell, [ts], initial_state=lstm_state, dtype=tf.float32)
 
     #     # initialize variables
     #     init_op = tf.global_variables_initializer()
     #     sess.run(init_op)
 
-    #     print(sess.run(res))
+    #     print(sess.run(ts))
         
 
 
@@ -158,10 +168,9 @@ if __name__ == "__main__":
     batch_size       = 5
     step_size        = 10
     lstm_hidden_size = 7
-    ts_len           = 3
 
     with tf.Session() as sess:
         
-        pprnn = PointProcessRNN(step_size, lstm_hidden_size, ts_len)
+        pprnn = PointProcessRNN(step_size, lstm_hidden_size)
         
         pprnn.train(sess, batch_size)
